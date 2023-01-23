f7252f4abc5670be160600a8a8ced060232120ef
Matthew Wisdom <matthewwisdom11@gmail.com>
2023-01-23 05:31:56 +0100

    Add configs

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+client_id = "d6DaAmZMBNwRgASmELix-A"
+client_secret = os.getenv("BOT_CLIENT_SECRET")
+user = os.getenv("BOT_USER")
+password = os.getenv("BOT_PASS")
+user_agent = "script_by_u/king_of_gazorpazorp"
+redirect_uri = "http://localhost:8080"
+rounds = 5
+round_interval = 7 * 86400
